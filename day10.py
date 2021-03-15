6b8047c3f7d3ca7362ad02d95558c66558060c5a
Kristian Fenn <kristianfenn1@gmail.com>
2021-03-15 21:30:06 +0000

    Tidy up day 10 a little

--- day10.py
+++ day10.py
@@ -1,7 +1,3 @@
-from collections import namedtuple
-
-LookaheadValue = namedtuple('LookaheadValue', ['index', 'value'])
-
 def part_1(input):
     input = [int(x) for x in input]
     input.append(0)
@@ -9,8 +5,6 @@ def part_1(input):
     input.sort()
 
     differences = [second - first for first, second in zip(input[:-1], input[1:])]
-
-    print(list(zip(input[1:], differences)))
 
     return len([x for x in differences if x == 1]) * len([x for x in differences if x == 3])
 
@@ -25,19 +19,18 @@ def part_2(input):
     paths_to_end[last_index] = 1
     lookahead_size = 3
 
-    for idx in range(last_index - 1, -1, -1):
+    for root_idx, root_value in reversed(list(enumerate(input[:-1]))):
         # this guff just calculates the window of values we could potentially get to from the value at the current index.
         # we start at the current index, because then enumerate helpfully gives us the offset from the current index to the lookahead value.
-        lookahead_end = min(idx + lookahead_size, last_index) + 1
-        lookahead = [LookaheadValue(idx + offset, value) for offset, value in enumerate(input[idx:lookahead_end])]
-        lookahead_root = lookahead[0]
+        lookahead_end = min(root_idx + lookahead_size, last_index) + 1
+        lookahead = [(root_idx + offset, value) for offset, value in enumerate(input[root_idx:lookahead_end])]
 
-        # find all the values in the lookahead that we can potentially reach.
+        # find all the indexes from the values in the lookahead that we can potentially reach.
         # discard the current node from the lookahead, as we can't traverse to ourselves.
-        values_reachable_from_root = [x for x in lookahead[1:] if x.value - lookahead_root.value <= 3]
+        indexes_reachable_from_root = [idx for idx, value in lookahead[1:] if value - root_value <= 3]
 
         # the possible paths to the end are just the sum of all the paths to the end for the nodes we can reach.
-        paths_to_end[idx] = sum(paths_to_end[paths.index] for paths in values_reachable_from_root)
+        paths_to_end[root_idx] = sum(paths_to_end[idx] for idx in indexes_reachable_from_root)
 
     return paths_to_end[0]
 
