3ecd6e2e6f1ab5abe7e3984d8b7b35bb377114cd
Kristian Fenn <kristianfenn1@gmail.com>
2021-03-22 21:11:04 +0000

    day 14

---
+++ day14.py
@@ -0,0 +1,81 @@
+import re
+
+parse_mem_re = r"mem\[([0-9]+)] = ([0-9]+)"
+bit_len = 36
+
+def to_bit_array(num):
+    bit_string = bin(num)[2:]
+    bin_string_len = len(bit_string)
+
+    if bin_string_len < bit_len:
+        bit_string = ("0" * (bit_len - bin_string_len)) + bit_string
+    elif bin_string_len > bit_len:
+        bit_string = bit_string[:bit_len]
+        
+    return [x for x in bit_string]
+
+def from_bit_array(bit_array):
+    as_string = "".join(bit_array)
+    return int(as_string, 2)
+
+def apply_simple_mask(num, mask):
+    num_bit_array = to_bit_array(num)
+    for idx,bit in enumerate(mask):
+        if bit == "0":
+            num_bit_array[idx] = "0"
+        elif bit == "1":
+            num_bit_array[idx] = "1"
+    return from_bit_array(num_bit_array)
+
+def apply_floating_mask(num, mask):
+    num_bit_array = to_bit_array(num)
+    for idx,bit in enumerate(mask):
+        if bit == "1":
+            num_bit_array[idx] = "1"
+        elif bit == "X":
+            num_bit_array[idx] = "X"
+    permutations = generate_floating_permutations(num_bit_array)
+    return [from_bit_array(x) for x in permutations]
+
+def generate_floating_permutations(bit_array):
+    result = []
+    for idx,bit in enumerate(bit_array):
+        if bit == "X":
+            with_zero = bit_array.copy()
+            with_zero[idx] = "0"
+            with_one = bit_array.copy()
+            with_one[idx] = "1"
+            result += generate_floating_permutations(with_zero)
+            result += generate_floating_permutations(with_one)
+            return result
+    
+    return ["".join(bit_array)]
+
+def part_1(input):
+    mask = []
+    memory = {}
+
+    for line in input:
+        if line.startswith("mask"):
+            mask = [x for x in line[7:]]
+        elif line.startswith("mem"):
+            address,val = [int(x) for x in re.match(parse_mem_re, line).groups()]
+            result = apply_simple_mask(val,mask)
+            memory[address] = result
+    
+    return sum(value for value in memory.values())
+
+def part_2(input):
+    mask = []
+    memory = {}
+
+    for line in input:
+        if line.startswith("mask"):
+            mask = [x for x in line[7:]]
+        elif line.startswith("mem"):
+            address,val = [int(x) for x in re.match(parse_mem_re, line).groups()]
+            addresses = apply_floating_mask(address, mask)
+            for address in addresses:
+                memory[address] = val
+    
+    return sum(value for value in memory.values())
---
+++ day14_test.py
@@ -0,0 +1,56 @@
+import unittest
+from day14 import part_1, part_2, generate_floating_permutations, apply_floating_mask
+
+input_file = open("day14_input.txt", "r")
+input = input_file.read().splitlines()
+input_file.close()
+
+example_input = """\
+mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+mem[8] = 11
+mem[7] = 101
+mem[8] = 0
+""".splitlines()
+
+class Part1Tests(unittest.TestCase):
+    def test_part_1_example_input(self):
+        result = part_1(example_input)
+        self.assertEqual(result, 165)
+
+    def test_part_1_real_input(self):
+        result = part_1(input)
+        self.assertEqual(result, 11884151942312)
+
+part_2_example_input = """\
+mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1
+""".splitlines()
+
+class Part2Tests(unittest.TestCase):
+    def test_floating_permutations(self):
+        bit_array = [x for x in "000000000000000000000000000000X1101X"]
+        result = generate_floating_permutations(bit_array)
+        assert len(result) == 4
+        assert "000000000000000000000000000000011010" in result
+        assert "000000000000000000000000000000011011" in result
+        assert "000000000000000000000000000000111010" in result
+        assert "000000000000000000000000000000111011" in result
+
+    def test_apply_floating_mask(self):
+        result = apply_floating_mask(42, "000000000000000000000000000000X1001X")
+        assert len(result) == 4
+        assert 26 in result
+        assert 27 in result
+        assert 58 in result
+        assert 59 in result
+
+    def test_part_2_example_input(self):
+        result = part_2(part_2_example_input)
+        self.assertEqual(result, 208)
+
+    def test_part_2_real_input(self):
+        result = part_2(input)
+        self.assertEqual(result, 2625449018811)
+    
